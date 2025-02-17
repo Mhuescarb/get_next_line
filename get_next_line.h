3a9e85818a784661cfe2189c1059878756549664
mhuescar <mhuescar@student.42malaga.com>
2025-02-17 17:12:43 +0100

    17 febrero

---
+++ get_next_line.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mhuescar <mhuescar@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/17 16:20:37 by mhuescar          #+#    #+#             */
+/*   Updated: 2025/02/17 17:08:02 by mhuescar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+
+/*insertar funciones aquí*/
+
+
+# include <unistd.h>
+# include <stdlib.h>
+
+#endif
